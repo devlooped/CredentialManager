0253c5b5293386fd0c34b84bbe28280e92ae2b99
Daniel Cazzulino <daniel@cazzulino.com>
2021-05-17 15:59:08 -0300

    Initial commit of solution and CredentialStore factory
    Adds the submodule and configurations for it to build properly.

---
+++ readme.md
@@ -0,0 +1,3 @@
+# CredentialManager
+
+Packages the [Microsoft.Git.CredentialManager](https://github.com/microsoft/Git-Credential-Manager-Core/tree/main/src/shared/Microsoft.Git.CredentialManager) cross-platform implementation for Windows, macOS and Linux for use as a library.
---
+++ src/CredentialManager/CredentialStore.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Microsoft.Git.CredentialManager
+{
+    /// <summary>
+    /// Provides the factory method <see cref="Create"/> to instantiate the 
+    /// right implementation of <see cref="ICredentialStore"/> appropriate for 
+    /// the current platform.
+    /// </summary>
+    public static class CredentialStore
+    {
+        /// <summary>
+        /// Creates the right implementation of <see cref="ICredentialStore"/> 
+        /// appropriate for the current platform.
+        /// </summary>
+        /// <param name="appPath">Optional application path.</param>
+        /// <returns>The <see cref="ICredentialStore"/>.</returns>
+        public static ICredentialStore Create(string? appPath = default)
+            => new CommandContext(appPath ?? GetApplicationPath()).CredentialStore;
+
+        // See GCM's Program.cs
+        static string GetApplicationPath()
+        {
+            // Assembly::Location always returns an empty string if the application was published as a single file
+#pragma warning disable IL3000
+            bool isSingleFile = string.IsNullOrEmpty(Assembly.GetEntryAssembly()?.Location);
+#pragma warning restore IL3000
+
+            // Use "argv[0]" to get the full path to the entry executable - this is consistent across
+            // .NET Framework and .NET >= 5 when published as a single file.
+            string[] args = Environment.GetCommandLineArgs();
+            string candidatePath = args[0];
+
+            // If we have not been published as a single file on .NET 5 then we must strip the ".dll" file extension
+            // to get the default AppHost/SuperHost name.
+            if (!isSingleFile && Path.HasExtension(candidatePath))
+            {
+                return Path.ChangeExtension(candidatePath, null);
+            }
+
+            return candidatePath;
+        }
+    }
+}
