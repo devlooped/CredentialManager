abe8df8035e381643b6c1a4ff5e30bc1157a4b9e
Daniel Cazzulino <daniel@cazzulino.com>
2023-03-22 15:31:13 -0300

    Bump NuGetizer and SponsorLink

--- src/Analyzer/SponsorLinker.cs
+++ src/Analyzer/SponsorLinker.cs
@@ -4,13 +4,12 @@ using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace Devlooped.CredentialManager;
 
-[Generator]
 [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic, LanguageNames.FSharp)]
 class SponsorLinker : SponsorLink
 {
     public SponsorLinker() : base(SponsorLinkSettings.Create(
         "devlooped", "CredentialManager", "Devlooped.CredentialManager",
-        version: new Version(ThisAssembly.Info.Version).ToString(2)
+        version: new Version(ThisAssembly.Info.Version).ToString(3)
 #if DEBUG
         , quietDays: 0
 #endif
